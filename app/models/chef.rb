2abe3f76d95802cb9138236c874afaeb9bf30d49
foosp8 <foosp8@hotmail.com>
2019-04-12 09:55:41 +0800

    add one to many relationship between recipe & chef

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,6 +3,8 @@ before_save { self.email = email.downcase }
   validates :chefname, presence: true, length: {maximum: 30}
       VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
+            format: { with: VALID_EMAIL_REGEX }
+  validates :email, uniqueness: { case_sensitive: false }
+
+  has_many :recipes
 end
