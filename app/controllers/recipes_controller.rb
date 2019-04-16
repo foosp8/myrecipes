e1faf2336b9b355142988a0ff70b749fa012c46a
foosp8 <foosp8@hotmail.com>
2019-04-16 08:45:54 +0800

    added new  create function

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,33 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.first
+    if @recipe.save
+      flash[:sucess] = "recipe was created sucessfully"
+      redirect_to recipe_path(@recipe)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+      @recipe = Recipe.find(params[:id])
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'recipes', to: 'recipes#index', as: :recipes
+
+  get 'recipes/new', to: 'recipes#new', as: :new_recipe
+
+  get 'recipes/:id', to: 'recipes#show', as: :recipe
+
+  get 'recipes/:id/edit', to: 'recipes#edit', as: :edit_recipe
+
+  post 'recipes', to: 'recipes#create'
+
+  patch 'recipes/id', to: 'recipes#update'
+
+
   root "pages#home"
   get 'pages/home', to: 'pages#home'
 
